5b131433fff6ddd77a45694050b5f34d168efc43
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-07-24 12:50:18 +0400

    Initial Commit

---
+++ CollectionView+GrandCentralDispath(GCD)/Sources/Modules/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CollectionView+GrandCentralDispath(GCD)
+//
+//  Created by Artem Mushtakov on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
